c21538fa271b041400e0e44660ef37bfa517a9d4
최민제 <louis9982.dev@gmail.com>
2022-11-02 14:30:08 +0900

    google drive method added

--- src/modelLoader.py
+++ src/modelLoader.py
@@ -1,5 +1,6 @@
 import wandb
 import torch
+import gdown
 from transformers import(
     AutoModelForSequenceClassification,
     AutoModelForCausalLM,
@@ -7,7 +8,6 @@ from transformers import(
 )
 
 def loadModelForSequenceClassification(model_name, artifact_version, model_path = '/best_model_at_end/pytorch_model.bin'):
-    wandb.login()
     run = wandb.init()
     artifact = run.use_artifact(artifact_version, type='model')
     artifact_dir = artifact.download()
@@ -17,14 +17,35 @@ def loadModelForSequenceClassification(model_name, artifact_version, model_path 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return (model, tokenizer)
 
+def gdLoadModelForSequenceClassification(model_name, artifact_version = '1IBoQCiD9cW9D7AmcE3BbLDG6TRm9zA02'):
+    url = 'https://drive.google.com/uc?id=' + artifact_version
+    artifact_dir = 'modelSequenceClassification.bin'
+
+    gdown.download(url, artifact_dir, quiet=False)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model.load_state_dict(torch.load(artifact_dir))
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return (model, tokenizer)
+
 def loadModelForCasualLM(model_name, artifact_version, model_path = '/best_model_at_end/pytorch_model.bin'):
-    wandb.login()
     run = wandb.init()
     artifact = run.use_artifact(artifact_version, type='model')
     artifact_dir = artifact.download()
 
     model = AutoModelForCausalLM.from_pretrained(model_name)
     model.load_state_dict(torch.load(artifact_dir+model_path))
+    tokenizer = AutoTokenizer.from_pretrained(model_name, 
+                                              bos_token='</s>', eos_token='</s>', unk_token='<unk>',
+                                              pad_token='<pad>', mask_token='<mask>')
+    return (model, tokenizer)
+
+def gdLoadModelForCausalLM(model_name, artifact_version = '1_PJoPgx0gsE2HrXdXmcW-uhPh5R3CgxC'):
+    url = 'https://drive.google.com/uc?id=' + artifact_version
+    artifact_dir = 'modelCausalLM.bin'
+
+    gdown.download(url, artifact_dir, quiet=False)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model.load_state_dict(torch.load(artifact_dir))
     tokenizer = AutoTokenizer.from_pretrained(model_name, 
                                               bos_token='</s>', eos_token='</s>', unk_token='<unk>',
                                               pad_token='<pad>', mask_token='<mask>')
